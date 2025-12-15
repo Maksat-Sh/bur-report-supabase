086bc1e498930e5d5860214304ed492420b3edd9
Maksat-Sh <maksshakh@mail.ru>
2025-12-15 15:33:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 import os
 from fastapi import FastAPI
 from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import text
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set")
 
-# 🔴 ВАЖНО: ssl="require"
 engine = create_async_engine(
     DATABASE_URL,
     echo=True,
-    connect_args={
-        "ssl": "require"
-    }
+    connect_args={"ssl": True}
 )
 
 app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
-    # Просто проверка соединения, БЕЗ create_all
     async with engine.connect() as conn:
-        await conn.execute("SELECT 1")
+        await conn.execute(text("SELECT 1"))
 
 @app.get("/")
 async def root():
